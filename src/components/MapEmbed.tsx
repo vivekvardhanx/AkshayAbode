ea9bebffbc1426ecc8b9078221e349da2ac520db
vivek vardhan <samlibraryvp@gmail.com>
2025-08-01 17:46:03 +0000

    17.418400, 78.222166  this is the location

--- src/components/MapEmbed.tsx
+++ src/components/MapEmbed.tsx
@@ -2,7 +2,7 @@ export function MapEmbed() {
     return (
         <div className="aspect-video w-full overflow-hidden rounded-lg border">
             <iframe
-                src="https://maps.google.com/maps?q=boys%20hostel%20delhi&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                src="https://maps.google.com/maps?q=17.418400,78.222166&t=&z=15&ie=UTF8&iwloc=&output=embed"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
