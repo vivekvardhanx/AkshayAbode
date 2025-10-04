d7aec880c0ca0bbc5d10a8ce7d016d75175f3aa3
vivek vardhan <samlibraryvp@gmail.com>
2025-10-04 18:54:33 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,8 @@ import { ArrowRight, Wifi, UtensilsCrossed, WashingMachine, Tv, Bath, Zap, Dropl
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { getRooms, type Room } from "@/lib/rooms";
 import { RoomCard } from "@/components/RoomCard";
+import { MirrorIcon } from "@/components/icons/MirrorIcon";
 
-
-const MirrorIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M14.5 2h-5L7 7v10c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V7l-2.5-5z" />
-      <path d="M7 21h10" />
-      <path d="M12 2v2" />
-    </svg>
-  );
 
 const amenities = [
   { icon: Wifi, text: "Free WiFi" },
---
+++ src/components/icons/MirrorIcon.tsx
@@ -0,0 +1,18 @@
+export const MirrorIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M14.5 2h-5L7 7v10c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V7l-2.5-5z" />
+      <path d="M7 21h10" />
+      <path d="M12 2v2" />
+    </svg>
+);
